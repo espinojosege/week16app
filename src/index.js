d7512ee2964aaa1f1a3f2f95e46ec252bb5119ea
espinojosege <EspinojoseG@gmail.com>
2023-08-05 20:57:27 -0700

    first commit to week 16 final project

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Layout from "./pages/Layout";
 import Home from "./pages/Home";
+import Layout from "./pages/Layout";
 import Video from "./pages/Video";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
-import "https://maps.googleapis.com/maps/api/js?key=AIzaSyBWK76t8Q-Wb6Fxjijhz4VPwLkGtFmMa9M&callback=initMap&libraries=&v=weekly";
-
 export default function App() {
   return (
     <BrowserRouter>
